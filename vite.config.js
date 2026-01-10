42a24e982ce26b524605f5f827d019313cb84814
zturner1 <zturner14@gmail.com>
2026-01-10 15:13:54 -0500

    fix: close plugins array in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(),
+  plugins: [react()],
   base: '/Felicity-Sight-Words/',
   server: {
     host: true,
