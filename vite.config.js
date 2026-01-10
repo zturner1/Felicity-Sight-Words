b37c393be0a0566bf36e7b2c3858ed7481af7902
zturner1 <59305115+zturner1@users.noreply.github.com>
2026-01-10 14:48:07 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/felicity-sight-words/',
+  plugins: [react(),
+  base: '/Felicity-Sight-Words/',
   server: {
     host: true,
     port: 5173,
